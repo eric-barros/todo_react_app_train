ccb34b8863a0af3561fa916e650bda5e793df54f
eric-barros <e.mhz.barros@gmail.com>
2025-07-06 07:20:06 -0700

    App.test.js file modified with screen rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import App from './App'; // Import the component you want to test
 
-test('renders todo item with correct text', () => {
-    expect(screen.getByText(/My Tasks/i)).toBeInTheDocument();
+test('renders learn react link', () => { // Define your test case
+  render(<App />); // Render the component
+  const linkElement = screen.getByText(/My Tasks/i); // Query for an element
+  expect(linkElement).toBeInTheDocument(); // Assert its presence
 });
