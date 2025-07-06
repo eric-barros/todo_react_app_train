b28fccf863b0347c3321881e39f188cc8c4beca1
eric-barros <e.mhz.barros@gmail.com>
2025-07-06 07:13:36 -0700

    App.test.js file modified with screen rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,34 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import {
-	Text,
-	Group,
-	Card,
-	ActionIcon,
-	//Code,
-} from '@mantine/core';
-import { Trash } from 'tabler-icons-react';
 
 test('renders todo item with correct text', () => {
-    const task = { title: "Gardening", summary: "With my daughter" };
-    render(
-        <Card withBorder mt={'sm'}>
-            <Group position={'apart'}>
-                <Text weight={'bold'}>{task.title}</Text>
-                <ActionIcon
-                    onClick={() => {
-                        deleteTask(index);
-                    }}
-                    color={'red'}
-                    variant={'transparent'}>
-                    <Trash />
-                </ActionIcon>
-            </Group>
-            <Text color={'dimmed'} size={'md'} mt={'sm'}>
-                {task.summary
-                    ? task.summary
-                    : 'No summary was provided for this task'}
-            </Text>
-        </Card>
-    );
-    expect(screen.getByText(/Gardening/i)).toBeInTheDocument();
+    expect(screen.getByText(/My Tasks/i)).toBeInTheDocument();
 });
