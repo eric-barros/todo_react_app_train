624eeb0eb1466fb9c0601ba14f5fb695137aa1bc
eric-barros <e.mhz.barros@gmail.com>
2025-07-06 06:53:50 -0700

    App.test.js created with rendering

---
+++ src/App.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import TodoItem from './TodoItem';
+
+test('renders todo item with correct text', () => {
+    const task = { title: "Gardening", summary: "With my daughter" };
+    render(
+        <Card withBorder key={index} mt={'sm'}>
+            <Group position={'apart'}>
+                <Text weight={'bold'}>{task.title}</Text>
+                <ActionIcon
+                    onClick={() => {
+                        deleteTask(index);
+                    }}
+                    color={'red'}
+                    variant={'transparent'}>
+                    <Trash />
+                </ActionIcon>
+            </Group>
+            <Text color={'dimmed'} size={'md'} mt={'sm'}>
+                {task.summary
+                    ? task.summary
+                    : 'No summary was provided for this task'}
+            </Text>
+        </Card>
+    );
+    expect(screen.getByText(/Gardening/i)).toBeInTheDocument();
+});
