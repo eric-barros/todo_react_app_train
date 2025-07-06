241e1d6706366c58c2257b6d758bafd720312b98
eric-barros <e.mhz.barros@gmail.com>
2025-07-06 07:16:28 -0700

    App.test.js file modified with screen rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
 test('renders todo item with correct text', () => {
     expect(screen.getByText(/My Tasks/i)).toBeInTheDocument();
