03dd50617af259b4e21cd76179ff58f039a122d7
eric-barros <e.mhz.barros@gmail.com>
2025-07-06 07:29:35 -0700

    App.test.js file modified with screen rendering

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App'; // Import the component you want to test
-
-test('renders learn react link', () => { // Define your test case
-  render(<App />); // Render the component
-  const linkElement = screen.getByText(/My Tasks/i); // Query for an element
-  expect(linkElement).toBeInTheDocument(); // Assert its presence
-});
